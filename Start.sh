f2c7a74897ccaa69a72b14b42470aa777d3f5993
Daniel Woltering <daniel.woltering@iu.org>
2024-01-17 16:56:15 +0000

    Fix adjust readme

--- Start.sh
+++ Start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Build the Docker image
 docker build -t my_client_app .
 
-# Run the Docker container and connect it to the 'backend-network'
-docker run -d -it --network=audit-backend_backend-network --name my_client_container my_client_app
+docker run -d -it --network=isssecurity-audit-backend_backend-network --name my_client_container my_client_app
